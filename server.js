ddd905fe7722c4dec7c0d3d923da7ca05f6ff0e8
JoaoMarcusAndrade <jm0982642@gmail.com>
2025-11-25 22:31:05 -0300

    arrumando limite de req

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-app.use(express.json());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
  
 app.use('/', routes);
 
